4874dbe3cb4f9077738d9697a83f480dca5bef0b
Vivek Pandey <vivekpandey1596@gmail.com>
2018-03-22 17:34:01 +0530

    Added constructor overriding

---
+++ src/main/java/Employee.java
@@ -0,0 +1,11 @@
+class Employee{
+  int id;
+  String name;
+  float salary;
+  void insert(int i, String n, float s) {
+    id=i;
+    name=n;
+    salary=s;
+  }
+  void display(){System.out.println(id+" "+name+" "+salary);}
+}
